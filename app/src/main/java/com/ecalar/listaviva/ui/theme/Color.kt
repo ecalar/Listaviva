567a8ca60e0bc141ee2f7e7b165ac2518e9bfcd2
Enrique <enriquecalar@gmail.com>
2026-06-29 17:35:31 +0200

    chore(proyecto): inicializar proyecto Listaviva con Jetpack Compose

---
+++ app/src/main/java/com/ecalar/listaviva/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.ecalar.listaviva.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val GreenPrimary = Color(0xFF2E8B57)
+val OrangeSecondary = Color(0xFFFF8C42)
+val LightBackground = Color(0xFFF5F7FA)
+val DarkBackground = Color(0xFF121212)
